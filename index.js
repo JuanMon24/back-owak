41427040145ef0c98d8355e7f201501326e91e5a
JuanMon24 <juandamc2010@gmail.com>
2024-09-02 17:48:24 -0500

    config index.js

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
+// const express = require('express');
+// const jsonServer = require('json-server');
+
+// const app = express();
+// const router = jsonServer.router('db.json'); // Archivo JSON que actúa como base de datos
+// const middlewares = jsonServer.defaults();
+
+// app.use(middlewares);
+// app.use('/api', router);
+
+// // Configuración del puerto
+// const port = process.env.PORT || 3001;
+// app.listen(port, () => {
+//   console.log(`Servidor corriendo en el puerto ${port}`);
+// });
+
 const express = require('express');
 const jsonServer = require('json-server');
 
 const app = express();
-const router = jsonServer.router('db.json'); // Archivo JSON que actúa como base de datos
+const router = jsonServer.router({ mensajes: [] }); // Inicia con datos vacíos en memoria
 const middlewares = jsonServer.defaults();
 
 app.use(middlewares);
 app.use('/api', router);
 
 // Configuración del puerto
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Servidor corriendo en el puerto ${port}`);
 });
+
